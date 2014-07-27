fea8ccfe62f3d75543760521dd63239e313e641e
Jon <jdwarner@gmail.com>
2014-07-27 11:43:48 +0100

    feature complete

--- analyze_movies.py
+++ analyze_movies.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, Counter
 import sys
 
 
@@ -11,14 +12,23 @@ class MovieLensParser(object):
     ALLOWED_GROUPINGS = ["gender", "agegroup"]
     # orderings allowed
     ALLOWED_ORDERS = ["top", "bottom"]
-    # data files and how to handle them
+
+    # readable names for fields in the users.dat file
+    USER_GENDERS = {'F': 'Female', 'M': 'Male'}
+    USER_AGES =    {1:  "Under 18", 18:  "18-24", 25:  "25-34", 35:  "35-44",
+                    45:  "45-49", 50:  "50-55", 56:  "56+"}
 
     order = ''
     grouping = ''
-    num_results = 0
+    max_results = 0
     movies = {}
-    ratings = {}
-    users = {}
+    ratings = defaultdict(dict)
+    users = defaultdict(list)
+    # cache for use with decodeline
+    expected_parts = {}
+
+    # output message to display results in
+    output_msg = []
 
     def __init__(self, args):
         # this will check for Movie Lens files, load them into memory etc
@@ -30,8 +40,8 @@ class MovieLensParser(object):
         # files to load
         self.DATA_FILES = {
             "movies.dat": self.loadMovies,
-         #   "ratings.dat": "loadRatings",
-         #   "users.dat": "loadUsers",
+            "ratings.dat": self.loadRatings,
+            "users.dat": self.loadUsers,
         }
 
         for filename in self.DATA_FILES.keys():
@@ -49,14 +59,50 @@ class MovieLensParser(object):
         line_counter = 0
         for line in data_file:
             line_counter += 1
-            movie_details = line.split('::')
-            if len(movie_details) != 3:
-                line_error = 'Invalid data detected in movies.dat at line {0}, expected [{1}], got [{2}]'.format(
-                             line_counter, line, line_format
-                )
-                raise MovieLensParserFileError(line_error)
-
+            movie_details = self.decodeLine('movies.dat', line, line_counter, line_format)
             self.movies[movie_details[0]] = movie_details[1]
+
+    def loadRatings(self, data_file):
+        line_format = 'UserID::MovieID::Rating::Timestamp'
+        line_counter = 0
+        for line in data_file:
+            line_counter += 1
+            rating_details = self.decodeLine('ratings.dat', line, line_counter, line_format)
+            # ie ratings[user_id][movie_id] = rating
+            self.ratings[rating_details[0]][rating_details[1]] = rating_details[2]
+
+    def loadUsers(self, data_file):
+        # the most important loader since this will be the start point for our analysis
+        # load / key the data differently according to whether gender or agegroup was chosen
+        line_counter = 0
+        line_format = 'UserID::Gender::Age::Occupation::Zip-code'
+
+        for line in data_file:
+            line_counter += 1
+            user_details = self.decodeLine('users.dat', line, line_counter, line_format)
+
+            if self.grouping == 'gender':
+                # gender => list of user ids
+                self.users[user_details[1]].append(user_details[0])
+            elif self.grouping == 'agegroup':
+                # agegroup => list of userids
+                self.users[int(user_details[2])].append(user_details[0])
+            else:
+                raise MovieLensParserInputError('{0} is not a supported grouping'.format(self.grouping))
+        pass
+
+    def decodeLine(self, filename, line, line_counter, line_format):
+        # cache split of line format for speed
+        if filename not in self.expected_parts:
+            self.expected_parts[filename] = len(line_format.split('::'))
+
+        parts = line.split('::')
+        if len(parts) != self.expected_parts[filename]:
+            line_error = 'Invalid data detected in {0} at line {1}, expected [{2}], got [{3}]'.format(
+                         filename, line_counter, line_format, line
+            )
+            raise MovieLensParserFileError(line_error)
+        return parts
 
     def validateInput(self, shell_input):
         # validate input from the shell - must be in this format: (gender|agegroup) (top|bottom) <number>
@@ -70,18 +116,44 @@ class MovieLensParser(object):
             raise MovieLensParserInputError("{0} is not a valid order. Supported orders are: ({1})".format(
                                             self.order, ", ".join(MovieLensParser.ALLOWED_ORDERS)))
         try:
-            self.number = int(shell_input.pop(0))
+            self.max_results = int(shell_input.pop(0))
         except (TypeError, ValueError):
             raise MovieLensParserInputError('<number> must be an integer')
 
-        if self.number + 0 > MovieLensParser.MAX_MOVIES:
+        if self.max_results + 0 > MovieLensParser.MAX_MOVIES:
             raise MovieLensParserInputError("Please choose an number less than {0}".format(
                                             MovieLensParser.MAX_MOVIES))
 
     def getResults(self):
         # based on our validated parameters, return a set of results, formatted for output to shell
-        print self.movies
-        pass
+        # iterate through our groups, and assign the movies / ratings in them into the relevant buckets
+        for group in sorted(self.users.keys()):
+            group_name = self.USER_AGES[group] if self.grouping == 'agegroup' else self.USER_GENDERS[group]
+            bucketed = Counter()
+            # do a slice on our ratings to get the ratings for the set of users in this group
+            group_ratings = [self.ratings[x] for x in self.users[group]]
+            # iterate over each user's set of ratings
+            for rating_set in group_ratings:
+                for movie_id in rating_set.keys():
+                    bucketed[movie_id] += int(rating_set[movie_id])
+
+            if self.order == 'top':
+                ranked_movies = bucketed.most_common(self.max_results)
+            elif self.order == 'bottom':
+                ranked_movies = reversed(bucketed.most_common()[len(bucketed) - self.max_results:])
+            else:
+                raise MovieLensParserInputError('{0} is not a valid ordering'.format(self.order))
+
+            group_title = ['', '{0} ranked movies for group: {1}'.format(self.order.capitalize(), group_name), '']
+            self.output_msg.extend(group_title)
+
+            for movie in ranked_movies:
+                formatted = "{0} - total rating {1}".format(
+                    self.movies[movie[0]], movie[1]
+                )
+                self.output_msg.append(formatted)
+
+        return "\n".join(self.output_msg)
 
 
 # exceptions relating to input
